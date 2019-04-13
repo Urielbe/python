1cd8341e3f89dcfccceacdaf6e7076adb1b5eb50
Urielbe <benatar87@gmail.com>
2019-04-14 01:21:53 +0300

    homework

---
+++ fruit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+fruit=input("enter your fruit:")
+
+if(fruit == "banana"):
+   print("fruit is banana")
+elif(fruit == "aplle"):
+   print("fruit is aplle")
+else:
+   print("your fruit is not banana or aplle")
