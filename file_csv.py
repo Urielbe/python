afa08fc2c8357959e0d32bc07cdc66c7d119d7ab
Urielbe <benatar87@gmail.com>
2019-04-14 20:15:46 +0300

    homework

---
+++ file_csv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import time
+import csv
+
+with open('/home/urielb/Desktop/csv_file.csv') as c:
+    data = c.read()
+print(data)
+cdata = data['172.16.0.2']= int('192.168.1.100')
+
+print(cdata)
--- print_name.py
+++ print_name.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 
  x= input ("your first name:")
-
- if(fruit = "banana"):
-    print("fruit is banana")
-elif(fruit = "aplle"):
-    print("fruit is aplle")
-else:
-    print("your fruit is not banana or aplle")
-    
